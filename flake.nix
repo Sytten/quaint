816f67cfe8eed2776dc42b28bef2d796044a846c
Julius de Bruijn <julius@nauk.io>
2021-03-06 22:20:07 +0100

    Nix integration

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Rust build tools";
+  # Provides abstraction to boiler-code when specifying multi-platform outputs.
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          rust-analyzer
+          rustfmt
+          rustup
+          openssl
+          pkg-config
+          clangStdenv
+        ];
+      };
+    });
+}
